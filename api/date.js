f62a1d48e9e2846109921f54421c04f237349ca0
Frank Klingebiel <klingebiel@optimal-systems.de>
2019-06-11 16:10:08 +0200

    Test env

--- api/date.js
+++ api/date.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 const moment = require('moment');
 
 const app = express();
 
-app.use(helmet());
+//app.use(helmet());
 
 app.get('*', (req, res) => {
-  res.set('Content-Type', 'text/html');
-  const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-  res.status(200).send(currentTime);
+  //res.set('Content-Type', 'text/html');
+  //const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+  //res.status(200).send(currentTime);
+  res.set('Content-Type', 'application/json');
+  res.status(200).send(JSON.stringify(process.env));
 });
+
+app.get('/env', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.status(200).send(JSON.stringify(process.env));
+});
+
 
 module.exports = app;
